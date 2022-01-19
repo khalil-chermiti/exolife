e64013909ee199c321657c06c586f75886bb4404
khalil chermiti <khalil666chermiti@gmail.com>
2022-01-19 15:48:30 +0100

    versioning api v1

--- server/src/app.js
+++ server/src/app.js
@@ -2,23 +2,26 @@ const express = require("express") ;
 const path = require("path");
 const morgan = require("morgan") ;
 const cors = require("cors") ;
-const {planetsRouter} = require("./routes/planets/planets.router") ;
-const {launchesRouter} = require("./routes/launches/launches.router");
+const api = require("./routes/api")
 
 const app = express() ;
 
 // middlewares 
 app.use(cors({origin : "http://localhost:3000"}));
 
+// print log to the STDOUT
 app.use(morgan('tiny'));
+
 // parse into json 
 app.use(express.json());
 
 // routing 
+
+// mounting the react app 
 app.use(express.static(path.join(__dirname , '..' , 'public'))) ;
-app.use('/planets', planetsRouter) ;
-app.use('/launches',launchesRouter) ;
 app.get('/*' , (req ,res) => res.sendFile(path.join(__dirname , '..' , 'public' , 'index.html'))) ;
 
+// API V1  
+app.use('/v1' , api);
 
 module.exports = app ;
